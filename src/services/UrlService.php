46ea608dac6987fd17c23f93bb4624df53d15c5e
Luka Avramovic <avramoviclu@gmail.com>
2024-01-03 23:24:25 +0100

    Adds convertIdToShortUrl method.

--- src/services/UrlService.php
+++ src/services/UrlService.php
@@ -9,17 +9,33 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class UrlService
 {
-    public function shorten(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    public function shorten(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $response->getBody()->write("Hello world!");
         
         return $response;
     }
 
-    public function redirect(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    public function redirect(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $response->getBody()->write("Hello world!");
         
         return $response;
+    }
+
+    private function convertIdToShortUrl(int $id): string
+    {
+        $map = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+        $shortUrl = '';
+
+        while ($id) {
+            $shortUrl .= $map[$id % 62];
+            $id = intval($id / 62);
+        }
+
+        $shortUrl = strrev($shortUrl);
+
+        return $shortUrl;
     }
 }
